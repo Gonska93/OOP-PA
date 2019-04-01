700db5ae4635dc9f405365e864b4fb1657f6f2e7
Piotr <gonska9933@gmail.com>
2019-04-01 12:42:06 +0200

    Create project structure

---
+++ src/main/java/org/mediatheque/model/item/MediaItem.java
@@ -0,0 +1,45 @@
+package org.mediatheque.model;
+
+import java.util.Date;
+
+public class MediaItem {
+    private int id;
+    private String name;
+    private String author;
+    private Date releaseDate;
+
+
+    public MediaItem(int id, String name, String author, Date releaseDate) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
